19846dfafe1b2d27f53f43ebffbb5222bcf7fc0e
shantam <smravan@umich.edu>
2016-07-14 06:03:04 -0400

    finsed the root files

--- Analyze.c
+++ Analyze.c
@@ -22,11 +22,41 @@
 #include "LineFitter.h"
 #define PI 3.14159265
 
+double* topparams = new double(6);
+double* bottomparams = new double(6);
 
-const double* Fit(TGraph2D *tg);
+
+
+void Fit(TGraph2D *tg, int num);
+
+void Plot(TGraph2D *t1, TGraph2D *t2)
+{
+	TCanvas *c3 = new TCanvas("c3");
+	c3->cd();
+	TH2D *h2 = new TH2D("first","points",6000,-300,300,6000,-300,300);
+	h2->SetMinimum(-400);
+	h2->SetMaximum(400);
+
+	t1->SetHistogram(h2);
+	t1->SetMarkerStyle(34);
+	t1->SetMarkerSize(1);
+	t1->SetMarkerColor(2);
+	t1->Draw("P");
+
+	TCanvas *two =new TCanvas("c2");
+	two->cd();
+	//t2->SetHistogram(h2);
+	t2->SetMarkerStyle(34);
+	t2->SetMarkerSize(1);
+	t2->SetMarkerColor(2);
+	t2->Draw("P");
+}
 
 void Analyze()
-{
+{	
+	TGraph2D *impactpoints = new TGraph2D();
+	int impactpointscounter = 0;
+	ofstream fitinfo("fitdata.csv");
 	std::ifstream ifs("runinfo.csv",std::ifstream::in);
 	if(!ifs)
 		std::cout<<"error";
@@ -71,14 +101,9 @@ void Analyze()
 
 			i1 = 0;
 			i2 = 0;
-			const double* topparams = Fit(top);
-			const double* bottomparams = Fit(bot);
+			Fit(top,1);
+			Fit(bot,2);
 
-			delete top;
-			delete bot;
-
-			top = new TGraph2D();
-			bot = new TGraph2D();
 
 			if(!(topparams && bottomparams))
 			{
@@ -87,19 +112,55 @@ void Analyze()
 				cout<<i<<endl;
 				exit(1);
 			}
+			
 
-			XYZVector x1(topparams[3],topparams[4],topparams[5]);
-			XYZVector u1 = x1.Unit();
-			XYZVector x2(bottomparams[3],bottomparams[4],bottomparams[5]);
-			XYZVector u2 = x2.Unit();
+			XYZVector d1(topparams[3],topparams[4],topparams[5]);
+
+			XYZVector d2(bottomparams[3],bottomparams[4],bottomparams[5]);
+			//double dp = d1.Dot(d2);
+
+
+			XYZVector p1(topparams[0],topparams[1],topparams[2]);
+			XYZVector p2(bottomparams[0],bottomparams[1],bottomparams[2]);
+
+			XYZVector n1 = d1.Cross(d2);
+			XYZVector n2 = d2.Cross(n1);
+			XYZVector n3 = d1.Cross(n1);
+			XYZVector c2 = p2 + (((p1-p2).Dot(n3))/(d2.Dot(n3)))*(d2);
+			XYZVector c1 = p1 + (((p2-p1).Dot(n2))/(d1.Dot(n2)))*(d1);
+
+			XYZVector impactpt((c1.x()+c2.x())/2,(c1.y()+c2.y())/2,(c1.z()+c2.z())/2);
+			XYZVector u1 = d1.Unit();
+			XYZVector u2 = d2.Unit();
 
 			double dp = u1.Dot(u2);
 
-			Event e;
+			//fitinfo<<abs(dp)<<','<<acos(abs(dp)) * 180 / PI<<','<<(xd1+xd2)/2<<','<<(yd1+yd2)/2<<','<<(zd1+zd2)/2<<','<<endl;
 
-			e.val = dp;
+			if(abs(dp) < 0.9)
+			{
+				cout<<"error!!!"<<endl;
+				cout<<topparams[3]<<','<<topparams[4]<<','<<topparams[5]<<endl;
+				cout<<bottomparams[3]<<','<<bottomparams[4]<<','<<bottomparams[5]<<endl;
+				//cout<<x1.x()<<','<<x1.y()<<','<<x1.z()<<endl;
+				//cout<<x2.x()<<','<<x2.y()<<','<<x2.z()<<endl;
+				//Plot(top,bot);
+				return;
+			}
+
+
+			Event e(dp,impactpt);
 			//add closest point here
 			evtlist.push_back(e);
+
+			impactpoints->SetPoint(impactpointscounter,impactpt.x(),impactpt.y(),impactpt.z());
+			impactpointscounter++;
+
+			delete top;
+			delete bot;
+
+			top = new TGraph2D();
+			bot = new TGraph2D();
 		}
 		else if(z == 300)
 		{
@@ -131,15 +192,20 @@ void Analyze()
 
 	}
 
-	ofstream fitinfo("fitdata.csv");
-	for (std::vector<Event>::iterator i = evtlist.begin(); i != evtlist.end(); ++i)
+	for (std::vector<Event>::iterator it = evtlist.begin(); it != evtlist.end(); ++it)
 	{
-		fitinfo<<i->val<<","<<acos(i->val)  * 180.0 / PI<<endl;
+		fitinfo<<it->val<<','<<acos(it->val)*180/PI<<','<<it->closestpt.x()<<','<<it->closestpt.y()<<','<<it->closestpt.z()<<endl;
 	}
+	
+	impactpoints->SetMarkerStyle(34);
+	impactpoints->SetMarkerSize(1);
+	impactpoints->SetMarkerColor(2);
+	impactpoints->Draw("P");
+
 	fitinfo.close();
 }
 
-const double* Fit(TGraph2D *tg)
+void Fit(TGraph2D *tg, int num)
 {
 	LineFit lf(tg);
 
@@ -152,8 +218,22 @@ const double* Fit(TGraph2D *tg)
 	   ROOT::Math::Functor fcn(lf,6);
 	#endif
 
-    double paramstart[6] = {-10.,-10.,-10.,-10.,-10.,-10.};
+    
 
+    double* x,*y,*z;
+    x = tg->GetX();
+    y = tg->GetY();
+    z = tg->GetZ();
+
+    int lastpt = tg->GetN() - 1;
+
+    XYZVector firstpt(x[0],y[0],z[0]);
+    
+    XYZVector secondpt(x[lastpt],y[lastpt],z[lastpt]);
+
+    XYZVector dir = secondpt - firstpt; 
+
+    double paramstart[6] = {secondpt.x(),secondpt.y(),secondpt.z(),dir.x(),dir.y(),dir.z()};
 
    	fitter.SetFCN(fcn,paramstart);
 
@@ -163,8 +243,24 @@ const double* Fit(TGraph2D *tg)
 
    	if (!ok) {
    		
-   	   Error("line3Dfit","Line3D Fit failed");
-   	  return 0;
+   	   //Error("line3Dfit","Line3D Fit failed");
+   	  cout<<"trying again"<<endl;
+
+   	  XYZVector dir2(-dir.x(),-dir.y(),-dir.z());
+
+   	  double paramstart2[6] = {secondpt.x(),secondpt.y(),secondpt.z(),dir2.x(),dir2.y(),dir2.z()};
+
+   	  fitter.SetFCN(fcn,paramstart2);
+
+   	  for (int i = 0; i < 6; ++i) fitter.Config().ParSettings(i).SetStepSize(0.01);
+
+   	  bool ok2 = fitter.FitFCN();
+   		if(!ok2)
+   		{
+   			cout<<"fialed"<<endl;
+   			exit(1);
+   		}
+
    	}
 
    	const ROOT::Fit::FitResult & result = fitter.Result();
@@ -172,5 +268,20 @@ const double* Fit(TGraph2D *tg)
    	std::cout << "Total final distance square " << result.MinFcnValue() << std::endl;
    	result.Print(std::cout);
    	const double * parFit = result.GetParams();
-   	return parFit;
+
+   	if(num == 1)
+   	{
+   		for(int i = 0; i<6; i++)
+   		{
+   			topparams[i] = parFit[i];
+   		}
+   	}
+   	else
+   	{
+   		for(int i = 0; i<6; i++)
+   		{
+   			bottomparams[i] = parFit[i];
+   		}
+   	}
+
 }
--- DrawTest.c
+++ DrawTest.c
@@ -24,7 +24,7 @@ int DrawTest()
 	if(!ifs)
 		std::cout<<"error";
 
-/*	TCanvas* c1 = new TCanvas("c2");
+	/*TCanvas* c1 = new TCanvas("c1");/*
    Double_t w = gPad->GetWw()*gPad->GetAbsWNDC();
    Double_t h = gPad->GetWh()*gPad->GetAbsHNDC();
    Double_t xmin = -150;
@@ -113,7 +113,7 @@ int DrawTest()
 
 	//dt->Draw("P");
 
-	dt2->Draw("P");
+	//dt2->Draw("P");
 	
 	
 	//return 0;
@@ -133,7 +133,8 @@ int DrawTest()
 	double paramstart[6] = {-10.,-10.,-10.,-10.,-10.,-10.};
 
 	int n = 10000;
-/*
+
+
 
 	fitter.SetFCN(fcn,paramstart);
 
@@ -152,13 +153,21 @@ int DrawTest()
 	result.Print(std::cout);
 
 	//dt->Draw("P");
-	dt2->Draw("P");
+	//dt2->DrawClone("P same");
+	//c1->Modified();
+	//c1->Update();
 
 	//gr->Draw("p0");
 
 	// get fit parameters
 	
-	const double * parFit = result.GetParams();
+	double * parFit1  = new double(6);
+	const double* fitparms = result.GetParams();
+
+	for (int i = 0; i < 6; ++i)
+	{
+		parFit1[i] = fitparms[i];
+	}
 
 	
 	double t0 = 50;
@@ -167,14 +176,14 @@ int DrawTest()
 	for (int i = 0; i <n;++i) {
 	   double t = t0+ dtl*i/n;
 	   double x,y,z;
-	   line1(t,parFit,x,y,z);
+	   line1(t,parFit1,x,y,z);
 	   //cout<<x<<','<<y<<','<<z<<endl;
 	   l->SetPoint(i,x,y,z);
 	}
 	l->SetLineColor(kRed);
 	//h2->Draw();
 	//l->Draw("same");
-	*/
+	
 
 	fitter.SetFCN(fcn2,paramstart);
 
@@ -188,7 +197,13 @@ int DrawTest()
 	}
 
 	const ROOT::Fit::FitResult & result2 = fitter.Result();
-	parFit = result2.GetParams();
+	double * parFit2  = new double(6);
+	fitparms = result2.GetParams();
+
+	for (int i = 0; i < 6; ++i)
+	{
+		parFit2[i] = fitparms[i];
+	}
 
 	std::cout << "Total final distance square " << result2.MinFcnValue() << std::endl;
 	result2.Print(std::cout);
@@ -198,22 +213,74 @@ int DrawTest()
 	for (int i = 0; i <n;++i) {
 	   double t = t0+ dtl*i/n;
 	   double x,y,z;
-	   line1(t,parFit,x,y,z);
+	   line1(t,parFit2,x,y,z);
 	   //cout<<x<<','<<y<<','<<z<<endl;
 	   l2->SetPoint(i,x,y,z);
 	}
 	l2->SetLineColor(kRed);
-	l2->Draw("same");
+	//l2->Draw("same");
 
+	ROOT::Fit::Fitter  fitter2;
 
-	
+	ImpactPointFit ipf(parFit1,parFit2);
 
+	#ifdef __CINT__
+	   ROOT::Math::Functor ipffcn(&ifp,2,"ImpactPointFit");
+	#else
+	   ROOT::Math::Functor ipffcn(ipf,2);
+	#endif	
+
+	double sparams[2] = {-100,100};
+
+	fitter2.SetFCN(ipffcn,sparams);
+
+	for (int i = 0; i < 2; ++i) fitter2.Config().ParSettings(i).SetStepSize(0.01);
+
+	bool fin = fitter2.FitFCN();
+
+	if (!fin)
+	{
+		cout<<"failed"<<endl;
+		return 1;
+	}
+
+	const ROOT::Fit::FitResult & resultipf = fitter2.Result();
+
+	std::cout << "Total final distance square " << resultipf.MinFcnValue() << std::endl;
+	resultipf.Print(std::cout);
+
+	const double *ipfparams = resultipf.GetParams();
+
+	cout<<ipfparams[0]<<','<<ipfparams[1]<<endl;
 
 /*	TBox *tb = new TBox(10,10,20,20);
 	tb->SetFillColor(2);
 	tb->Draw("same");
 	return 0;*/
 
+	TPolyMarker3D *tpd = new TPolyMarker3D();
+	//double x,y,z;
+	line1(ipfparams[0],parFit1,x,y,z);
+	cout<<x<<','<<y<<','<<z<<','<<endl;
+	tpd->SetPoint(0,x,y,z);
+	line1(ipfparams[1],parFit2,x,y,z);
+	tpd->SetPoint(1,x,y,z);
 
+	cout<<x<<','<<y<<','<<z<<','<<endl;
+
+	XYZVector p1(parFit1[0],parFit1[1],parFit1[2]);
+	XYZVector p2(parFit2[0],parFit2[1],parFit2[2]);
+
+	XYZVector d1(parFit1[3],parFit1[4],parFit1[5]);
+	XYZVector d2(parFit2[3],parFit2[4],parFit2[5]);
+
+	XYZVector n1 = d1.Cross(d2);
+	XYZVector n2 = d2.Cross(n1);
+	XYZVector n3 = d1.Cross(n1);
+	XYZVector c2 = p2 + (((p1-p2).Dot(n3))/(d2.Dot(n3)))*(d2);
+	XYZVector c1 = p1 + (((p2-p1).Dot(n2))/(d1.Dot(n2)))*(d1);
+	
+	cout<<c1.x()<<','<<c1.y()<<','<<c1.z()<<','<<endl;
+	cout<<c2.x()<<','<<c2.y()<<','<<c2.z()<<','<<endl;
 	return 0;
 }
